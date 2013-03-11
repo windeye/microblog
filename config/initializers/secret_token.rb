eb244a2ffb741cca76e5d2e4d285ebbb729dbb3c
windeye <leokmax@me.com>
2013-03-12 05:45:22 +0800

    init project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Microblog::Application.config.secret_token = '0c10cdc584c478c0826cb4b463df6c0b8aaba2148e22274bb6ce0c20de16412db223d6980b79cf7154058028311aec4b193b96c8790ef80af05e97681d83928e'
